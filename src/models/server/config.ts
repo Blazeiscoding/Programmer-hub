d99144d2d5c8e1aa45cf5a8262fff5c544d67979
Nikhil Rathore <nikhilrathore52297@gmail.com>
2024-10-08 02:35:12 +0530

    complete server appwrite

--- src/models/server/config.ts
+++ src/models/server/config.ts
@@ -6,3 +6,9 @@ client
     .setEndpoint(env.appwrite.endpoint) // Your API Endpoint
     .setProject(env.appwrite.projectId) // Your project ID
     .setKey(env.appwrite.apiKey) // Your secret API key
+;
+const databases = new Databases(client);
+const storage = new Storage(client);
+const avatars = new Avatars(client);
+const users = new Users(client);
+export {client,databases,storage,avatars,users}
