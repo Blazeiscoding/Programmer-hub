af09c38097684075e32078b022cf93b096efd446
Nikhil Rathore <nikhilrathore52297@gmail.com>
2024-10-08 08:50:12 +0530

    bugs

--- src/models/client/config.ts
+++ src/models/client/config.ts
@@ -1,11 +1,15 @@
-import { Client, Account, Avatars,Databases,Storage } from 'appwrite';
-import env from '@/app/env';
-const client = new Client()
-    .setEndpoint('env.endpoint') // Replace with your Appwrite endpoint
-    .setProject('env.projectId'); // Replace with your project ID
+import env from "@/app/env";
 
-const databases = new Databases(client);
-const storage = new Storage(client);
-const avatars = new Avatars(client);
+import { Client, Account, Avatars, Databases, Storage } from "appwrite";
+
+const client = new Client()
+    .setEndpoint(env.appwrite.endpoint) // Your API Endpoint
+    .setProject(env.appwrite.projectId); // Your project ID
+
+const databases = new Databases(client)
 const account = new Account(client);
-export {client,databases,storage,avatars,account}
+const avatars = new Avatars(client);
+const storage = new Storage(client);
+
+
+export { client, databases, account, avatars, storage}
